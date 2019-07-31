7d7a6b980a1ea48c7cd7110a0e89fe7fd134c5c0
reyanrd93 <yylee.rey@gmail.com>
2019-07-31 11:14:43 +0800

    Updates

---
+++ precache-manifest.1c3e203509991dc073721ca14fd5afba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc8bbf7ae96442c0cfbcbd5af258fcc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "649b3f2bcd963d82460b",
+    "url": "./static/css/main.a1de87e6.chunk.css"
+  },
+  {
+    "revision": "2dcbcc17331046968ab1",
+    "url": "./static/js/2.ff0bacdf.chunk.js"
+  },
+  {
+    "revision": "649b3f2bcd963d82460b",
+    "url": "./static/js/main.c2bc0704.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
